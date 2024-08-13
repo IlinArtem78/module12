cb0dd62b1a49d4c3450980cb131027721de54294
IlinArtem78 <artem.ilin1994@gmail.com>
2024-08-13 10:55:53 +0300

    Задание 12.1.3

--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Runtime.Serialization.Formatters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Сколько элементов будет в массиве?");
+        int count = int.Parse(Console.ReadLine());
+        string[] array = new string[count];
+        for (int i = 0; i < count; i++) {
+            array[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Все элементы записаны"); 
+    }
+}
