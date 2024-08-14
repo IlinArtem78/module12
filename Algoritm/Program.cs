794cc8d29e8c8f45a7c138cbe35665df8d36d91e
IlinArtem78 <artem.ilin1994@gmail.com>
2024-08-14 09:14:40 +0300

    Задание 12.1.5

--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -4,14 +4,61 @@ using System.Runtime.Serialization.Formatters;
 
 class Program
 {
-    static void Main(string[] args)
+   public static void Main(string[] args)
     {
-        Console.WriteLine("Сколько элементов будет в массиве?");
-        int count = int.Parse(Console.ReadLine());
-        string[] array = new string[count];
-        for (int i = 0; i < count; i++) {
-            array[i] = Console.ReadLine();
+       int k = 0;
+
+        List<User> users = new List<User>(5);
+        while (k < users.Count)
+        { 
+            User user = new User();
+            Console.WriteLine("Введите имя:");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("Введите логин:");
+            user.Login = Console.ReadLine();
+            
+            user.IsPremium = true;
+            Console.WriteLine($"Премиум подписа: {user.IsPremium}");
+            k++;
+            users.Add(user);
         }
-        Console.WriteLine("Все элементы записаны"); 
+
+        foreach (var user in users)
+        {
+           
+           Console.WriteLine(user.Login, user.Name, user.IsPremium);
+
+        }
+      //  User user1 = new User();
+       // user1.Name = Console.ReadLine(); 
+       // Console.WriteLine(user1.Name);
+     //   if (user1.IsPremium != true)
+       // {
+       //     ShowAds(); 
+       // }
+    
     }
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
+    }
+
 }
+
+class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
+
