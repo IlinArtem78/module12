9a5d9493651044fbb2ad47a01159740e847455a5
IlinArtem78 <artem.ilin1994@gmail.com>
2024-08-14 10:40:50 +0300

    12.3.1

---
+++ Algoritm/BinarSearch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm
+{
+    public class BinarSearch
+    {
+        static int BinarySearch(int value, int[] array, int left, int right)
+        {
+
+            var middle = (left + right) / 2;
+
+            var midElement = array[middle];
+
+            if (midElement == value)
+            {
+                return middle;
+            }
+            else if (left < right)
+            {
+                if (value < midElement)
+                {
+                    return BinarySearch(value, array, left, middle - 1);
+                }
+                else
+                {
+                    return BinarySearch(value, array, middle + 1, right);
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        //searchedValue - искомое значение, arr[] - массив, first - первый индекс массива (обычнно 0. ), last - последний индекс массива (array.length - 1)  
+        static int TaskBinarySearch(int searchedValue, int[] arr, int first, int last) 
+        {
+            while(first <= last)
+            {
+                //индекс среднего элемента
+                var middle = (first + last) / 2;
+
+                if (searchedValue == arr[middle])
+                {
+                    return middle;
+                }
+                else if (searchedValue < arr[middle])
+                {
+                    //сужаем рабочую зону массива с правой стороны
+                    last = middle - 1;
+                }
+                else
+                {
+                    //сужаем рабочую зону массива с левой стороны
+                    first = middle + 1;
+                }
+            }
+            //ничего не нашли
+            return -1;
+        }
+    }
+}
--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -1,16 +1,14 @@
 ﻿
-
 using System.Runtime.Serialization.Formatters;
-
 class Program
 {
-   public static void Main(string[] args)
+    public static void Main(string[] args)
     {
-       int k = 0;
+        int k = 0;
 
         List<User> users = new List<User>(5);
         while (k < users.Count)
-        { 
+        {
             User user = new User();
             Console.WriteLine("Введите имя:");
             user.Name = Console.ReadLine();
@@ -29,31 +27,32 @@ class Program
 
         foreach (var user in users)
         {
-             Console.WriteLine(user.Login, user.Name, user.IsPremium);
+            Console.WriteLine(user.Login, user.Name, user.IsPremium);
         }
-      
 
-    static void ShowAds()
-    {
-        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-        // Остановка на 1 с
-        Thread.Sleep(1000);
 
-        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-        // Остановка на 2 с
-        Thread.Sleep(2000);
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
 
-        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-        // Остановка на 3 с
-        Thread.Sleep(3000);
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
     }
-
 }
 
 class User
-{
-    public string Login { get; set; }
-    public string Name { get; set; }
-    public bool IsPremium { get; set; }
-}
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
 
